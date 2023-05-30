557cfa0d2da6d51e4fde8668ab2d28e851b0da17
Junaid-ahmed58 <junaid.ahmed@xprolabs.com>
2023-05-30 14:20:57 +0500

    Implemented destroy action for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -40,6 +40,15 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def destroy
+    @portfolio = Portfolio.find(params[:id])
+    @portfolio.destroy
+
+    respond_to do |format|
+      format.html { redirect_to portfolios_path,notice: "Portfolio was successfully destroyed." }
+    end
+  end
+
   def portfolio_params
     params.require(:portfolio).permit(:title, :subtitle, :body)
   end
