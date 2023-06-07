9b70b0781946517fe850eeca99c9b82f856bd08d
Junaid-ahmed58 <junaid.ahmed@xprolabs.com>
2023-06-07 13:12:21 +0500

    Implemented view helper for authenthication link in blog and portfolio

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def authentication_helper
+    if current_user.is_a?(User) 
+      button_to  'Logout', destroy_user_session_path, method: :delete
+    else
+      (link_to 'register', new_user_registration_path) +
+      "<br>".html_safe +
+      (link_to 'login', new_user_session_path) 
+    end 
+  end
 end
